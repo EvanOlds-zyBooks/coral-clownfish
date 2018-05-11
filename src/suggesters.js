891983744299fd117f3810c08711ead72db6227d
Evan Olds <32916715+EvanOlds-zyBooks@users.noreply.github.com>
2018-05-11 15:02:22 -0700

    Update suggesters.js
    Shows my suggester in action, with the 2 different modes (in order, or fish first)

--- src/suggesters.js
+++ src/suggesters.js
@@ -1,6 +1,9 @@
 import RandomSuggester from './suggesters/random';
+import EvanOldsSuggester from './suggesters/EvanOldsSuggest.js';
 
 // Reference your suggester class here using an import statment like above.
 
 // Add a new instance of your suggester here. It will then show up in game. 
-export default [ new RandomSuggester('Scott\'s randomizer') ];
+export default [ new RandomSuggester('Scott\'s randomizer'),
+                new EvanOldsSuggester("Evan Olds - in order", "inorder"),
+                new EvanOldsSuggester("Evan Olds - fish first", "fishfirst")];
